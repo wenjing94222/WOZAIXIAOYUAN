06c4233de99c23112c6eddda9013a08896e1c625
MIKUCHINCHAN <90995793+MIKUCHINCHAN@users.noreply.github.com>
2022-12-10 17:32:02 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -70,7 +70,7 @@ class processJson:
         url = 'https://apis.map.qq.com/ws/geocoder/v1/?address=' + parse.quote(
             address) + '&key=A3YBZ-NC5RU-MFYVV-BOHND-RO3OT-ABFCR'
         print(requests.get(url).text)
-    
+        print(requests.get(url).request.headers)
         jr = json.loads(requests.get(url).text)['result']['location']
         url = 'https://apis.map.qq.com/ws/geocoder/v1/?key=A3YBZ-NC5RU-MFYVV-BOHND-RO3OT-ABFCR&location=' + str(
             jr['lat']) + ',' + str(jr['lng'])
